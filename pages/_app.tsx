a07531040c9730c0a8744ac025cee5f137c06f8a
Roman Gladyshev <romagladyshev@gmail.com>
2021-08-05 16:53:03 +0600

    Init commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,28 @@
 import '../styles/globals.css'
+import '../styles/article.scss';
 import type { AppProps } from 'next/app'
+import { initializeApp } from "firebase/app"
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "metawall.firebaseapp.com",
+    databaseURL: "https://metawall-default-rtdb.firebaseio.com",
+    projectId: "metawall",
+    storageBucket: "metawall.appspot.com",
+    messagingSenderId: "726458653556",
+    appId: "1:726458653556:web:625dde4387a7a27b5aaa21",
+    measurementId: "G-F5TE4VKEKP"
+  }
+
+  try {
+    initializeApp(firebaseConfig);
+  } catch(err){
+    if (!/already exists/.test(err.message)) {
+      console.error('Firebase initialization error', err.stack)}
+  }
+
   return <Component {...pageProps} />
 }
 export default MyApp
---
+++ pages/article/[id].tsx
@@ -0,0 +1,143 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useState } from 'react';
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useRouter } from 'next/router';
+import Link from 'next/link'
+import moment from 'moment';
+import Countdown from 'react-countdown';
+import Head from 'next/head';
+
+const Icon = () => (
+  <svg width="40" height="30" viewBox="0 0 40 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" clipRule="evenodd" d="M6.66659 20.5708L13.3333 18.1963V-5H6.66659V20.5708ZM20.0001 15.8219L26.6666 13.4475V35H33.3333V11.0731L26.6667 13.4475V-5H20V15.8219L13.3334 18.1963V35H20.0001V15.8219ZM40.0001 8.69863L33.3334 11.0731V-5H40.0001V8.69863ZM0 35H6.66667V20.5708L0 22.8539V35Z" fill="white"/>
+  </svg>
+);
+
+const Burger = () => (
+  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 16H28" stroke="white" strokeWidth="2" strokeLinejoin="round"/>
+    <path d="M4 6.66666H28" stroke="white" strokeWidth="2" strokeLinejoin="round"/>
+    <path d="M4 25.3333H28" stroke="white" strokeWidth="2" strokeLinejoin="round"/>
+  </svg>
+);
+
+const Info = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+    <path d="M12 18L12 10" stroke="white" strokeWidth="1.5" strokeLinejoin="round"/>
+    <path d="M12 7H12.01" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+  </svg>
+)
+
+const Article = () => {
+  const [data, setData] = useState(null as any);
+  const [pref, setPref] = useState(null as any);
+  const [isShowMore, setShowMore] = useState(false);
+
+  const router = useRouter()
+  const db = getDatabase();
+
+  useEffect(() => {
+    const auctionsRef = ref(db, 'Auctions/');
+    onValue(auctionsRef, (snapshot) => {
+      const data = snapshot.val();
+      setData(data);
+    });
+    const prefRef = ref(db, 'Preferences/');
+    onValue(prefRef, (snapshot) => {
+      const pref = snapshot.val();
+      setPref(pref);
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  let pageData;
+
+  if(data) {
+    pageData = data.find((d:any) => d.ID == router.query.id)
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Metawall</title>
+      </Head>
+      <header className="header">
+        <Link href="https://metawall.io/">
+          <a>
+            <Icon />
+          </a>
+        </Link>
+        <Burger />
+      </header>
+      {pageData && pref && (
+        <section className="article">
+          <div className="articleImage">
+            <Link href={pageData.StreamURL}>
+              <a style={{display: "flex", width: "100%"}} target="_blank">
+                <img src={pageData.ArtworkPreviewImg} alt="Preview" />
+              </a>
+            </Link>
+            {pageData.StreamStatus === "Active" && (
+              <div className="articleImageStatus">
+                LIVE
+              </div>
+            )}
+          </div>
+          <div className="articleInfo">
+            <div style={{overflowY: "auto"}}>
+              <h3 className="articleInfoTitle">{pageData.ArtworkTitle}</h3>
+              {!isShowMore ? (
+                <p className="articleInfoDesc">{pageData.ArtworkDescription.slice(0, 200)} ... <span onClick={() => setShowMore(true)}>Read More</span></p>
+                ) : (
+                <p className="articleInfoDesc">{pageData.ArtworkDescription} <span onClick={() => setShowMore(false)}>Read Less</span></p>
+              )}
+              <div className="articleInfoAuthor">
+                <div className="articleInfoAuthorInfo">
+                  <img src={pageData.ArtistImg} alt="author" />
+                  <div className="articleInfoAuthorInfoText">
+                    <p>Author</p>
+                    <p>{pageData.ArtistName}</p>
+                  </div>
+                </div>
+                <Link href={pageData.ArtistURL}>
+                  <a target="_blank">
+                    <Info />
+                  </a>
+                </Link>
+              </div>
+              <h4 className="articleInfoSubtitle">Activity</h4>
+              <div className="articleInfoBids">
+                  {pageData.Bids.filter((bid: any) => bid.ApprovedByAdmin).map((bid: any, i: number) => (
+                    <div key={i} className="articleInfoBidsBid">
+                      <p><span className="articleInfoBidsSum">{bid.BidAmountETH} ETH</span> by <span className="articleInfoBidsNum">{bid.BidderWallet.slice(0, 11)}...{bid.BidderWallet.slice(40, 42)}</span></p>
+                      <p>{moment(bid.Date).format("YY.MM.DD HH:SS")}</p>
+                    </div>
+                  ))}
+              </div>
+            </div>
+            <div className="articleInfoAction">
+              <div className="articleInfoActionInfo">
+                <div className="articleInfoActionInfoBlock">
+                  <p>Current Bid</p>
+                  <p>{pageData.CurrentBidAmountETH} ETH <span>${(pageData.CurrentBidAmountETH * pref.ETHUSD).toFixed(3)}</span></p>
+                </div>
+                <div className="articleInfoActionInfoBlock">
+                  <p>Auction ending in</p>
+                  <Countdown daysInHours date={new Date(pageData.DateEnd)} />
+                </div>
+              </div>
+              <Link href={pageData.BidFormURL}>
+                  <a className="articleInfoActionButton" target="_blank">
+                    Place Bid
+                  </a>
+              </Link>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
+  )
+}
+
+export default Article
